1b2a09c4306cb97f2e42bfb2989b61f6a99e9731
Francesc <franhumbert@gmail.com>
2026-01-26 22:58:23 +0100

    Factory modification from the ListProjectsFactory.php file

--- backend/src/database/factories/ListProjectsFactory.php
+++ backend/src/database/factories/ListProjectsFactory.php
@@ -22,6 +22,7 @@ class ListProjectsFactory extends Factory
         $languages = LanguageEnum::values();
 
         return [
+            'owner_id' => \App\Models\User::factory(),
             'title' => $this->faker->sentence(3),
             'time_duration' => $this->faker->word(),
             'language_backend' => $this->faker->randomElement($languages),
