2e1db1e325139e37dc2855ba4e58ee173413197b
mstne03 <mserraitorne@gmail.com>
2026-01-27 10:56:18 +0100

    fix: modify skeleton padding

--- frontend/src/components/resources/ResourcesSkeleton.tsx
+++ frontend/src/components/resources/ResourcesSkeleton.tsx
@@ -1,10 +1,10 @@
 const ResourceCardSkeleton = () => {
     return (
-        <div  data-testid="resource-card-skeleton" className="flex flex-col w-full p-6 ring ring-gray-900/5 rounded-2xl shadow-md bg-gray-100">
+        <div  data-testid="resource-card-skeleton" className="flex flex-col w-full py-4 px-4 ring ring-gray-900/5 rounded-2xl shadow-md bg-gray-100">
             <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between animate-pulse">
                 <div className="flex flex-col sm:flex-row gap-1">
                 <div className="flex flex-col flex-1 gap-1 mt-2 sm:mt-0">
-                    <div className="h-6 w-90 max-w-full rounded bg-gray-300" />
+                    <div className="h-6 w-96 max-w-full rounded bg-gray-300" />
                     <div className="h-3 w-80 max-w-full mt-2 rounded bg-gray-300" />
                     <div className="flex flex-wrap items-center gap-3 sm:gap-4 mt-2">
                     <div className="flex items-center gap-1">
