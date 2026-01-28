017317364289cccacbc2999f539f104f2fba693d
GerardAsuncion <asunciongerard90@gmail.com>
2026-01-28 13:15:39 +0100

    feat: iconUtils now includes typescript

--- frontend/src/utils/iconUtils.ts
+++ frontend/src/utils/iconUtils.ts
@@ -4,8 +4,9 @@ import angular from "../assets/angular.svg";
 import javascript from "../assets/javascript.svg";
 import python from "../assets/pythonVector.svg";
 import java from "../assets/logo-java 1.svg";
+import typescript from "../assets/typescript.svg";
 
-export const displayLanguageIcon = (language: string | undefined | null) => {
+export const displayLanguageIcon = (language: string | undefined | null): string => {
   if (!language) return ""; 
   
   switch (language.toLowerCase()) {
@@ -15,6 +16,7 @@ export const displayLanguageIcon = (language: string | undefined | null) => {
     case "react": return react;
     case "angular": return angular;
     case "php": return php;
+    case "typescript": return typescript;
     default: return ""; 
   }
 };
