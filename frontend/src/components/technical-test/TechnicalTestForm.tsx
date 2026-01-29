fcd408f37adcbe157be9eeaa828d769afca47e12
jenifer5 <sonnia-55@hotmail.com>
2026-01-29 13:52:02 +0100

    Corrected placeholders in the test content section

--- frontend/src/components/technical-test/TechnicalTestForm.tsx
+++ frontend/src/components/technical-test/TechnicalTestForm.tsx
@@ -50,12 +50,15 @@ export const TechnicalTestForm = () => {
             languages={contentForTechnicalTest}
           />
 
+          {/* DURADA */}
           <div className="flex flex-col px-10 mt-8">
-            <label className="block mb-2 font-medium">Durada (minuts) *</label>
+            <label className="text-sm text-black font-medium mb-2">
+              Durada (minuts) *
+            </label>
             <input
               type="number"
               {...register("duration", { valueAsNumber: true })}
-              className="sm:w-1/2 p-2 border border-[#B91879] rounded-lg"
+              className="max-w-[482px] p-2 text-base border border-gray-300 rounded-lg focus:border-[#B91879] outline-none"
               min="1"
               placeholder="Ex: 60"
             />
@@ -68,11 +71,14 @@ export const TechnicalTestForm = () => {
             )}
           </div>
 
+          {/* DIFICULTAT */}
           <div className="flex flex-col px-10 mt-8">
-            <label className="block mb-2 font-medium">Dificultat *</label>
+            <label className="text-sm text-black font-medium mb-2">
+              Dificultat *
+            </label>
             <select
               {...register("difficulty")}
-              className="sm:w-1/2 p-2 border border-[#B91879] rounded-lg bg-white"
+              className="max-w-[482px] p-2 text-base border border-gray-300 rounded-lg bg-white focus:border-[#B91879] outline-none"
             >
               <option value="">Selecciona una dificultat</option>
               <option value="easy">Fàcil</option>
