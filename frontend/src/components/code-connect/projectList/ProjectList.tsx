8803d51041db71726920f456909b8cbbfd9fa158
BauveR <rick.bauve@gmail.com>
2026-02-01 19:17:41 +0100

    Extract Ui from projectlist, empty state like tje thecnicaltes

--- frontend/src/components/code-connect/projectList/ProjectList.tsx
+++ frontend/src/components/code-connect/projectList/ProjectList.tsx
@@ -1,39 +1,42 @@
-import ProjectCard from "../projectCard/ProjectCard";
-import type { Project } from "../projectCard/types/projectTypes";
-import projectsData from "../../../moock/projects.json";
-
-function ProjectList({
-  onCardClick,
-  filter,
-}: {
-  onCardClick?: (id: number) => void;
-  filter?: string | null;
-}) {
-  const projects = (projectsData as Project[]).filter((p) => {
-    if (!filter) return true;
-    const f = filter.toLowerCase();
-    return (
-      p.frontend?.tech?.toLowerCase() === f ||
-      p.backend?.tech?.toLowerCase() === f
-    );
-  });
-
-  return (
-    <>
-      <h2 className="text-2xl font-bold  py-4 sm:py-6 text-black sm:mb-4">
-        Llista de projectes
-      </h2>
-      <div className="grid justify-center ml-3 sm:ml-0 gap-1 sm:gap-10 grid-cols-[repeat(auto-fill,minmax(300px,1fr))] w-full">
-        {projects.map((project) => (
-          <ProjectCard
-            key={project.id}
-            project={project}
-            onClick={onCardClick}
-          />
-        ))}
-      </div>
-    </>
-  );
-}
-
-export default ProjectList;
+import { useState, useEffect } from "react";                                                                               
+import projectsData from "../../../moock/projects.json";                                                                   
+import { useMinLoading } from "../../../hooks/useMinLoading";                                                              
+import ProjectListUI from "./ProjectListUI";                                                                               
+import type { Project } from "../projectCard/types/projectTypes";                                                          
+                                                                                                                             
+  function ProjectList({                                                                                                     
+    onCardClick,                                                                                                             
+    filter,                                                                                                                  
+  }: {                                                                                                                       
+    onCardClick?: (id: number) => void;                                                                                      
+    filter?: string | null;                                                                                                  
+  }) {                                                                                                                       
+    const [isLoading, setIsLoading] = useState(true);                                                                        
+    const [error, setError] = useState<Error | null>(null);                                                                  
+    const showLoader = useMinLoading(isLoading);                                                                             
+                                                                                                                             
+    useEffect(() => {                                                                                                        
+      const timer = setTimeout(() => setIsLoading(false), 1500);                                                             
+      return () => clearTimeout(timer);                                                                                      
+    }, []);                                                                                                                  
+                                                                                                                             
+    const projects = (projectsData as Project[]).filter((p) => {                                                             
+      if (!filter) return true;                                                                                              
+      const f = filter.toLowerCase();                                                                                        
+      return (                                                                                                               
+        p.frontend?.tech?.toLowerCase() === f ||                                                                             
+        p.backend?.tech?.toLowerCase() === f                                                                                 
+      );                                                                                                                     
+    });                                                                                                                      
+                                                                                                                             
+    return (                                                                                                                 
+      <ProjectListUI                                                                                                         
+        projects={projects}                                                                                                  
+        showLoader={showLoader}                                                                                              
+        error={error}                                                                                                        
+        onCardClick={onCardClick}                                                                                            
+      />                                                                                                                     
+    );                                                                                                                       
+  }                                                                                                                          
+                                                                                                                             
+  export default ProjectList; 
