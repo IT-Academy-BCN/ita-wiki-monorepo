9d292fee5be5d5390af7d21d98796b6d6b06f983
GerardAsuncion <asunciongerard90@gmail.com>
2026-02-06 19:47:37 +0100

    fix: removed ResourcesList component from ResourcesPage, commented everything that will be useful when replacing divs for real components

--- frontend/src/pages/ResourcesPage.tsx
+++ frontend/src/pages/ResourcesPage.tsx
@@ -2,11 +2,11 @@ import { useEffect } from "react";
 import { useParams, useNavigate } from "react-router";
 import { categories } from "../data/categories";
 import { ResourcesFiltersProvider } from "../context/ResourcesFiltersContext";
-import { useResources } from "../context/ResourcesContext";
-import { ResourcesList } from "../components/resources/ResourcesList";
+// import { useResources } from "../context/ResourcesContext";
+// import { ResourcesLayout } from "../components/resources/ResourcesLayout";
 
 const ResourcesPage = () => {
-  const { resources } = useResources();
+  // const { resources } = useResources();
   const { category } = useParams();
   const navigate = useNavigate();
 
@@ -23,7 +23,8 @@ const ResourcesPage = () => {
         <div>Javascript, PHP, Java, BBDD, Python</div>
         <div>Ordenar, filtros</div>
       </div>
-      <ResourcesList resources={resources} category={category} />
+      <div>Lista de recursos</div>
+      {/* <ResourcesLayout resources={resources} category={category} /> */}
     </ResourcesFiltersProvider>
   );
 };
