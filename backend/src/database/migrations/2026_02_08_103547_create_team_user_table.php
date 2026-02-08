049b3b8533903eb34ea0c95fbcdc13310fb262f2
Francesc <franhumbert@gmail.com>
2026-02-08 12:03:02 +0100

    add tickets table migration

--- backend/src/database/migrations/2026_02_08_103547_create_team_user_table.php
+++ backend/src/database/migrations/2026_02_08_103547_create_team_user_table.php
@@ -13,9 +13,12 @@ return new class extends Migration
     {
         Schema::create('team_user', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('team_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('team_id')->constrained('teams')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
+
+            $table->unique(['team_id', 'user_id']);
+            
         });
     }
 
