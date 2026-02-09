5fc361fb0732c9ebc4d40b90126d24c1e08c4fa9
Marc Sanchez <mks4nch3z@gmail.com>
2026-02-09 12:02:38 +0100

    feature: create Controller & Request files

---
+++ backend/src/app/Http/Controllers/TicketController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ticket;
+use Illuminate\Http\Request;
+
+class TicketController extends Controller{
+
+    public function index(){
+
+        $tickets = Ticket::all();
+
+        return response()->json([
+            'success' => true,
+            'data' => $tickets
+        ]);
+    }
+
+    public function create(){
+
+        
+    }
+}
+?>
