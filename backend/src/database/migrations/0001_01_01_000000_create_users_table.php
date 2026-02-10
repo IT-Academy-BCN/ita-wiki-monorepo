ae273101fb158a70fa5b4ceff6260ef204381b56
Marc Sanchez <mks4nch3z@gmail.com>
2026-02-10 13:11:08 +0100

    Reintroducir fields for secure checks

--- backend/src/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/src/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
             $table->id();
             $table->bigInteger('github_id')->unsigned()->unique();
             $table->string('github_user_name')->nullable();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
             $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
