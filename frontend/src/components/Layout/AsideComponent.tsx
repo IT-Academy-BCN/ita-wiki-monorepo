8bfb1ff7c0361802607ef4b53d1661822e53ec2c
mstne03 <mserraitorne@gmail.com>
2026-02-11 13:14:09 +0100

    refactor: add logo to aside component and change to sticky

--- frontend/src/components/Layout/AsideComponent.tsx
+++ frontend/src/components/Layout/AsideComponent.tsx
@@ -1,8 +1,10 @@
-import { useLocation, useNavigate } from "react-router";
+import { Link, useLocation, useNavigate } from "react-router";
 import classNames from "classnames";
 
 import Bookmark from "../../assets/Bookmark.svg";
 import CreatedResources from "../../assets/CreatedResources.svg";
+
+import logoItAcademy from "../../assets/LogoItAcademy.svg";
 
 import { AsideNavbarData } from "./aside/asideContent.tsx";
 import { AsideNavbarLink } from "./aside/AsideNavbarLink";
@@ -15,8 +17,13 @@ const AsideComponent: React.FC = () => {
   const isPathActive = (path: string) => currentPath === path;
 
   return (
-    <aside className="flex flex-col px-6 lg:w-56 py-4">
-      <section>
+    <aside className="flex flex-col px-3 py-4 lg:min-w-70 sticky top-0 justify-between bg-white h-screen overflow-y-auto pt-5">
+      <section className="space-y-10">
+        <div className="flex justify-start">
+          <Link to="/">
+            <img src={logoItAcademy} alt="logo" width={"130px"} />
+          </Link>
+        </div>
         <ul className="py-6 space-y-3">
           {AsideNavbarData.map((data) => (
             <AsideNavbarLink
