b91727c099d2f2d083bda4b03219fa518410e293
mstne03 <mserraitorne@gmail.com>
2026-02-17 13:45:13 +0100

    feat: merge with new develop changes

--- frontend/src/components/Layout/AsideComponent.tsx
+++ frontend/src/components/Layout/AsideComponent.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation, useNavigate } from "react-router";
+import { Link, useLocation } from "react-router";
 import logoItAcademy from "../../assets/LogoItAcademy.svg";
 
 import { AsideNavbarData, AsideConfigData } from "./aside/asideContent.tsx";
@@ -12,27 +12,29 @@ const AsideComponent: React.FC = () => {
   const isPathActive = (path: string) => currentPath === path;
 
   return (
-    <aside className="flex flex-col px-3 py-4 lg:min-w-70 sticky top-0 justify-between bg-white h-screen overflow-y-auto pt-5">
-      <section className="space-y-10">
-        <div className="flex justify-start">
+    <aside className="flex flex-col lg:min-w-70 sticky top-0 bg-white h-screen overflow-hidden">
+      <section className="flex-1 flex flex-col min-h-0 px-3 pt-5 space-y-2">
+        <div className="flex justify-start flex-shrink-0">
           <Link to="/">
             <img src={logoItAcademy} alt="logo" width={"130px"} />
           </Link>
         </div>
-        <ul className="py-6 space-y-3">
-          {AsideNavbarData.map((data, i) => (
-            <AsideNavbarLink
-              key={i}
-              label={data.label}
-              path={data.ref}
-              isActive={isPathActive(data.ref)}
-              icon={data.icon}
-            />
-          ))}
-        </ul>
+        <nav className="flex-1 min-h-0 overflow-y-auto">
+          <ul className="space-y-3 text-[11px] sm:text-xs md:text-xs">
+            {AsideNavbarData.map((data, i) => (
+              <AsideNavbarLink
+                key={i}
+                label={data.label}
+                path={data.ref}
+                isActive={isPathActive(data.ref)}
+                icon={data.icon}
+              />
+            ))}
+          </ul>
+        </nav>
       </section>
 
-      <section className="py-6">
+      <section className="flex-shrink-0 px-3 pt-6 pb-4 text-[11px] sm:text-xs md:text-xs">
         <div className="-mx-4 border-t"></div>
         <ul className="py-6 space-y-3">
           {AsideConfigData.map((data) => (
