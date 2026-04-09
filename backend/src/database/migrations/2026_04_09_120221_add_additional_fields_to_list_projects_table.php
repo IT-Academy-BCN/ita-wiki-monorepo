b144dbed0fb908adc1ba900b18c780994fca0ce7
Victor C.B. <victor.charneco.barba@gmail.com>
2026-04-09 15:18:03 +0200

    fix(migrations): fixed typo in limit_date_inscription

--- backend/src/database/migrations/2026_04_09_120221_add_additional_fields_to_list_projects_table.php
+++ backend/src/database/migrations/2026_04_09_120221_add_additional_fields_to_list_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::table('list_projects', function (Blueprint $table) {
             $table->text('description')->nullable()->after('title');
             $table->date('limit_date_inscription')->nullable()->after('description');
-            $table->integer('dev_front_number')->nullable()->after('limite_date_inscription');
+            $table->integer('dev_front_number')->nullable()->after('limit_date_inscription');
             $table->integer('dev_back_number')->nullable()->after('dev_front_number');
         });
     }
