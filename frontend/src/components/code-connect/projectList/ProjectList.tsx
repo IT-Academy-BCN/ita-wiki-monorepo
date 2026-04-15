9f869f7f990ee62116cc9ec9ff12dd486d54b1df
Helene T <helene.teychene@gmail.com>
2026-04-15 11:02:48 +0200

    reimplemented previous code, just removing the timeout and initiating the loading variable to false

--- frontend/src/components/code-connect/projectList/ProjectList.tsx
+++ frontend/src/components/code-connect/projectList/ProjectList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useMinLoading } from "../../../hooks/useMinLoading";
 import projectsData from "../../../moock/projects.json";
 import type { Project } from "../projectCard/types/projectTypes";
@@ -14,21 +14,16 @@ function ProjectList({
   const [isLoading, setIsLoading] = useState(false);
   const error = null;
   const showLoader = useMinLoading(isLoading);
-  const [projects, setProjects] = useState<Project[]>([]);
 
-  useEffect(() => {
-    setIsLoading(true);
-    const projectsToShow = (projectsData as Project[]).filter((p) => {
-      if (!filter) return true;
-      const f = filter.toLowerCase();
-      return (
-        p.frontend?.tech?.toLowerCase() === f ||
-        p.backend?.tech?.toLowerCase() === f
-      );
-    });
-    setProjects(projectsToShow);
-    setIsLoading(false);
-  }, [filter]);
+
+  const projects = (projectsData as Project[]).filter((p) => {
+    if (!filter) return true;
+    const f = filter.toLowerCase();
+    return (
+      p.frontend?.tech?.toLowerCase() === f ||
+      p.backend?.tech?.toLowerCase() === f
+    );
+  });
 
   return (
     <ProjectListUI
