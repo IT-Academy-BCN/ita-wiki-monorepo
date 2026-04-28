04b7de3de1c744c3b077214e829e12057b682acf
Hebe Stark <hebe_s@hotmail.com>
2026-04-28 23:45:34 +0200

    feat(models): add Liga model (dependency from 302-model-creation)

---
+++ backend/src/app/Models/Liga.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Liga extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'points',
+    ];
+
+    protected $casts = [
+        'points' => 'integer',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+
+    }
+
+}
+
+
