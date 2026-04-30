519a1a75f531de879d222f039d1f94ef3d343246
Vicenç Sirvent <vrsirvent@gmail.com>
2026-04-30 11:56:49 +0200

    fix(ticketing): disable eslint unused-vars for stub parameter

--- frontend/src/api/endPointTickets.ts
+++ frontend/src/api/endPointTickets.ts
@@ -1,5 +1,6 @@
 import { IntCreateTicket, IntTicket } from "../types/ticketingTypes";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const createTicket = async (
   _data: IntCreateTicket,
 ): Promise<IntTicket> => {
