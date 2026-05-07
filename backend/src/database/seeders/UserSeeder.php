0d1833fb6324ec197ab0b1613edbf55bbda46da2
Miguel Montaño <miiguel2m@gmail.com>
2026-05-07 15:05:21 +0200

    add Mentor and Students to UserSeeder

--- backend/src/database/seeders/UserSeeder.php
+++ backend/src/database/seeders/UserSeeder.php
@@ -36,8 +36,47 @@ class UserSeeder extends Seeder
         );
         $admin->syncRoles(['admin']);
 
+        // Mentor
+        $mentor = User::firstOrCreate(
+            ['email' => 'mentor@test.com'],
+            [
+                'github_id'        => '33333333',
+                'github_user_name' => 'mentor_test',
+                'name'             => 'Mentor Test',
+                'password'         => Hash::make('password'),
+            ]
+        );
+        $mentor->syncRoles(['mentor']);
+
+        // Student 1
+        $student1 = User::firstOrCreate(
+            ['email' => 'student1@test.com'],
+            [
+                'github_id'        => '44444444',
+                'github_user_name' => 'student_test_1',
+                'name'             => 'Student Test 1',
+                'password'         => Hash::make('password'),
+            ]
+        );
+        $student1->syncRoles(['student']);
+
+        // Student 2
+        $student2 = User::firstOrCreate(
+            ['email' => 'student2@test.com'],
+            [
+                'github_id'        => '55555555',
+                'github_user_name' => 'student_test_2',
+                'name'             => 'Student Test 2',
+                'password'         => Hash::make('password'),
+            ]
+        );
+        $student2->syncRoles(['student']);
+
         $this->command->info('Users created with roles:');
         $this->command->info('superadmin@test.com / password → superadmin');
         $this->command->info('admin@test.com / password → admin');
+        $this->command->info('mentor@test.com / password → mentor');
+        $this->command->info('student1@test.com / password → student');
+        $this->command->info('student2@test.com / password → student');
     }
 }
