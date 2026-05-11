52efac4bf77bbf2bbf3fc7f491caa0e270b5611f
Vicenç Sirvent <vrsirvent@gmail.com>
2026-05-11 16:46:05 +0200

    feat: update LigaFactory with points_weekly, language, status and league_id

--- backend/src/database/factories/LigaFactory.php
+++ backend/src/database/factories/LigaFactory.php
@@ -1,22 +1,28 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
 namespace Database\Factories;
 
+use App\Enums\LanguageEnum;
+use App\Enums\LigaStatusEnum;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/** 
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Liga> 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Liga>
  */
 class LigaFactory extends Factory
 {
     public function definition(): array
     {
         return [
-            'user_id' => User::factory(),
-            'points' => fake()->numberBetween(0, 100),
+            'user_id'       => User::factory(),
+            'points'        => $this->faker->numberBetween(0, 100),
+            'points_weekly' => $this->faker->numberBetween(0, 100),
+            'language'      => $this->faker->randomElement(LanguageEnum::values()),
+            'status'        => $this->faker->randomElement(LigaStatusEnum::values()),
+            'league_id'     => $this->faker->numberBetween(1, 4),
         ];
     }
 }
