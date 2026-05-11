0363bf658ef03e2426112aec5e4046d5d40bd41f
Helene T <helene.teychene@gmail.com>
2026-05-11 23:13:26 +0200

    removed error related tests as per requested on review

--- frontend/src/hooks/__tests__/useGlobalRanking.test.ts
+++ frontend/src/hooks/__tests__/useGlobalRanking.test.ts
@@ -12,21 +12,18 @@ const mockData: LigaResponse = [
   {
     position: 1,
     user_id: 101,
+    username: "Sasha",
     points: 94,
+    weekly_points: 10,
     created_at: "2026-04-24T00:00:00Z",
     updated_at: "2026-04-24T00:00:00Z",
   },
   {
     position: 2,
     user_id: 102,
+    username: "Pixie",
     points: 88,
-    created_at: "2026-04-24T00:00:00Z",
-    updated_at: "2026-04-24T00:00:00Z",
-  },
-  {
-    position: 3,
-    user_id: 103,
-    points: 75,
+    weekly_points: 10,
     created_at: "2026-04-24T00:00:00Z",
     updated_at: "2026-04-24T00:00:00Z",
   },
@@ -52,31 +49,5 @@ describe("useGlobalRanking", () => {
     expect(fetchGlobalRanking).toHaveBeenCalledTimes(1);
     expect(result.current.error).toBeNull();
     expect(result.current.globalRanking).toEqual(mockData);
-  });
-
-  it("sets errorMessage in case of error", async () => {
-    vi.mocked(fetchGlobalRanking).mockRejectedValueOnce(
-      new Error("Network error"),
-    );
-    const { result } = renderHook(() => useGlobalRanking());
-
-    await waitFor(() => {
-      expect(result.current.isLoading).toBe(false);
-    });
-    expect(result.current.error).toBe("Network error");
-    expect(result.current.globalRanking).toHaveLength(0);
-  });
-
-  it("ignores AbortError and does not set errorMessage", async () => {
-    const abortError = new DOMException("Aborted", "AbortError");
-    vi.mocked(fetchGlobalRanking).mockRejectedValueOnce(abortError);
-    const { result } = renderHook(() => useGlobalRanking());
-
-    await waitFor(() => {
-      expect(result.current.isLoading).toBe(false);
-    });
-
-    expect(result.current.error).toBeNull();
-    expect(result.current.globalRanking).toEqual([]);
   });
 });
