b33aa00bd042cdf6df0535a92ebcabd47fbb35ee
Albert Valls <avalls89@gmail.com>
2026-05-13 21:12:08 +0200

    Remove out of scope test

--- frontend/src/api/__tests__/endPointLeague.test.ts
+++ frontend/src/api/__tests__/endPointLeague.test.ts
@@ -53,14 +53,4 @@ describe("addLeaguePoints", () => {
 
     expect(result).toEqual(mockResponse);
   });
-
-  it("Should throw an error when response is not ok", async () => {
-    fetchMock.mockResolvedValueOnce({
-      ok: false,
-    });
-
-    await expect(addLeaguePoints(1)).rejects.toThrow(
-      "Error adding league points",
-    );
-  });
 });
