6af37c29abea7b160ee660b58e5f6bd1a716ead1
Helene T <helene.teychene@gmail.com>
2026-05-14 16:58:55 +0200

    fix / simplify by restoring previous types and just adapting the LeagueListProps one

--- frontend/src/types/league.ts
+++ frontend/src/types/league.ts
@@ -4,8 +4,6 @@ export type Liga = {
   username: string;
   points: number;
   points_weekly: number;
-  status: string;
-  language: string;
   created_at: string;
   updated_at: string;
 };
@@ -13,7 +11,7 @@ export type Liga = {
 export type LigaResponse = Liga[];
 
 export type LeagueListProps = {
-  standings: Omit<Liga, "points_weekly">[];
+  standings: Omit<Ranking, "points_weekly" | "league_id">[];
 };
 
 export type Ranking = {
