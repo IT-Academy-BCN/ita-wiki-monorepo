c8223270439153f8352d7ccc935db28f348d221e
Helene T <helene.teychene@gmail.com>
2026-05-14 16:16:22 +0200

    fix/import

--- frontend/src/components/Leagues/__test__/GlobalRanking.test.tsx
+++ frontend/src/components/Leagues/__test__/GlobalRanking.test.tsx
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom/vitest";
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
-import { GlobalRanking } from "../../Leagues/GlobalRanking";
+import { GlobalRanking } from "../../leagues/GlobalRanking";
 
 describe("GlobalRanking", () => {
   it("renders the global ranking", () => {
