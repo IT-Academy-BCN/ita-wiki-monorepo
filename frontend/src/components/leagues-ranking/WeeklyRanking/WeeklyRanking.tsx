d85817e42685ed39dcdb2b32177747de73a30a80
Helene T <helene.teychene@gmail.com>
2026-05-15 14:21:47 +0200

    prettier fix

--- frontend/src/components/leagues-ranking/WeeklyRanking/WeeklyRanking.tsx
+++ frontend/src/components/leagues-ranking/WeeklyRanking/WeeklyRanking.tsx
@@ -10,7 +10,7 @@ export const WeeklyRanking = () => {
   useEffect(() => {
     getLeagueRanking()
       .then(setData)
-      .catch(() => { });
+      .catch(() => {});
   }, []);
   const LEAGUE_LABELS: Record<string, string> = {
     "1": "Or",
