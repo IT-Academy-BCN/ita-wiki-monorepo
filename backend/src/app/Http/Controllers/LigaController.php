777c7ea8efcdc95ecb519ceadbd07d0c6784003c
Helene T <helene.teychene@gmail.com>
2026-05-19 09:42:03 +0200

    added missing ,

--- backend/src/app/Http/Controllers/LigaController.php
+++ backend/src/app/Http/Controllers/LigaController.php
@@ -54,7 +54,7 @@ class LigaController extends Controller
                 'user_id'    => $entry->user_id,
                 'points'     => $entry->points,
                 'username'   => $entry->user->github_user_name,
-                'league_id'  => $entry->league_id
+                'league_id'  => $entry->league_id,
                 'created_at' => $entry->created_at,
                 'updated_at' => $entry->updated_at,
 
