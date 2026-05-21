89553be179bc92e42fb579ab3ac03788ed37884a
Vicenç Sirvent <vrsirvent@gmail.com>
2026-05-21 23:43:37 +0200

    feat(#594): add TicketComment type

--- frontend/src/types/ticketingTypes.ts
+++ frontend/src/types/ticketingTypes.ts
@@ -114,3 +114,14 @@ export interface UseTicketingGetAllState {
   isLoading: boolean;
   errorMessage: string | null;
 }
+
+export interface TicketComment {
+  id: number;
+  ticket_id: number;
+  user_id: number;
+  comment: string;
+  is_closing_comment: boolean;
+  created_at: string;
+  updated_at: string;
+  user: TicketUserData;
+}
