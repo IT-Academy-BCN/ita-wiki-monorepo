8450d2762cb989f7cbe08182ab852f35c281179f
toni <canillask8@gmail.com>
2026-05-22 17:28:26 +0200

    fix(#656): fix typo text-gray-foregorund to text-gray-foreground

--- frontend/src/components/atoms/ButtonComponent.tsx
+++ frontend/src/components/atoms/ButtonComponent.tsx
@@ -26,7 +26,7 @@ const basicCss =
   "block text-[14px] h-[41px] capitalize text-center rounded-[12px] outline-none cursor-pointer box-sizing w-full";
 const classList = {
   primary: `${basicCss} text-white min-w-[152px] bg-primary font-[600] hover:opacity-90 border-none`,
-  secondary: `${basicCss} border border-gray-foreground font-[600] text-gray-foregorund hover:bg-neutral-50 min-w-[138px]`,
+  secondary: `${basicCss} border border-gray-foreground font-[600] text-gray-foreground hover:bg-neutral-50 min-w-[138px]`,
   neutral: `${basicCss} bg-white text-gray-foreground font-[500] min-w-[138px] hover:bg-neutral-50`,
   github:
     "text-[var(--github-color)] bg-[var(--github-bg)]  justify-between max-w-60 hover:bg-[var(--github-color)] hover:text-[var(--github-bg)] hover:border-black outline-none cursor-pointer box-sizing",
