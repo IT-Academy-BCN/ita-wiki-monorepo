effd0c19decc4c2d5a4f7c2c94a753a81fd17e66
Helene T <helene.teychene@gmail.com>
2026-05-27 15:39:20 +0200

    added transition to reject button

--- frontend/src/components/code-connect/pendingRequests/RejectButton.tsx
+++ frontend/src/components/code-connect/pendingRequests/RejectButton.tsx
@@ -22,7 +22,7 @@ export const RejectButton = ({ project, contributor }: RejectButtonProps) => {
     <div className="flex items-center gap-2 flex-1 justify-end">
       <button
         onClick={handleReject}
-        className="rounded-lg border-2 border-stake color-stale text-sm font-bold py-1 px-5"
+        className="rounded-lg border-2 border-stake color-stale text-sm font-bold py-1 px-5 cursor-pointer hover:border-bg-primary hover:text-primary transition duration-300"
       >
         Rebutjar
       </button>
