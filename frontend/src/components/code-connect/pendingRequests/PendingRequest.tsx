28b50a25905149be692ef6f33b8baab7c1a1cac4
Miguel Montaño <miiguel2m@gmail.com>
2026-05-27 13:12:11 +0200

    feat: add technology icon centered in PendingRequest component

--- frontend/src/components/code-connect/pendingRequests/PendingRequest.tsx
+++ frontend/src/components/code-connect/pendingRequests/PendingRequest.tsx
@@ -3,8 +3,23 @@ import type { ApiContributor } from "../../../types/codeConnectTypes";
 
 interface PendingRequestProps {
   contributor: ApiContributor;
+  roleIcon?: string;
 }
 
-const PendingRequest: FC<PendingRequestProps> = () => null;
+const PendingRequest: FC<PendingRequestProps> = ({ contributor, roleIcon }) => (
+  <li className="flex items-center p-3 border-b last:border-b-0">
+    <div className="flex-1" />
+    <div className="flex-1 flex justify-center">
+      {roleIcon && (
+        <img
+          src={roleIcon}
+          alt={contributor.programming_role}
+          className="w-[70px] h-[35px] object-contain"
+        />
+      )}
+    </div>
+    <div className="flex-1" />
+  </li>
+);
 
 export default PendingRequest;
