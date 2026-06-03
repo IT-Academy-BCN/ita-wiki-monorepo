93c28929405ab06eae4503b818908962be990342
Clàudia López Torres <claudia_4bcn@hotmail.com>
2026-06-03 16:46:52 +0200

    adding modal

--- frontend/src/pages/LeaguesPage.tsx
+++ frontend/src/pages/LeaguesPage.tsx
@@ -4,20 +4,25 @@ import LeagueToggle, {
 } from "../components/leagues-ranking/LeagueToggle/LeagueToggle";
 import { GlobalRanking } from "../components/leagues-ranking/GlobalRanking/GlobalRanking";
 import { WeeklyRanking } from "../components/leagues-ranking/WeeklyRanking/WeeklyRanking";
-import UiButton from "../components/ui/shared-ui/UiButton";
+import GenericModal from "../components/ui/Modal/GenericModal";
 
 const LeaguesPage = () => {
   const [view, setView] = useState<LeagueView>("weekly");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div className="px-6 md:px-10 xl:px-20 2xl:px-6 flex flex-col gap-10">
       <div className="flex items-center justify-between w-full">
         <LeagueToggle view={view} onChange={setView} />
-        <UiButton variant="link" size="sm" onClick={() => {}}>
-          Veure el meu historial
-        </UiButton>
       </div>
       {view === "weekly" ? <WeeklyRanking /> : <GlobalRanking />}
+
+      <GenericModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title="El meu historial de punts"
+        size="lg"
+      />
     </div>
   );
 };
