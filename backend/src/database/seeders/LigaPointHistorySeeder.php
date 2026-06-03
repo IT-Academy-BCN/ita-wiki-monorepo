f4101e4773fc6a4b7c1bbf21b2eb4e5d8ef5be11
Miguel Montaño <miiguel2m@gmail.com>
2026-06-03 17:02:03 +0200

    feat: add LigaPointHistorySeeder

---
+++ backend/src/database/seeders/LigaPointHistorySeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\LigaPointHistory;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class LigaPointHistorySeeder extends Seeder
+{
+    public function run(): void
+    {
+        $activities = [
+            'Completed a resource',
+            'Submitted a project',
+            'Reviewed a peer project',
+            'Passed a weekly quiz',
+        ];
+
+        User::all()->each(function (User $user) use ($activities): void {
+            foreach (array_slice($activities, 0, rand(1, 4)) as $activity) {
+                LigaPointHistory::create([
+                    'user_id'  => $user->id,
+                    'points'   => rand(5, 50),
+                    'activity' => $activity,
+                ]);
+            }
+        });
+    }
+}
