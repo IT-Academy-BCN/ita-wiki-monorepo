8b4c63f7437c97b4aa354d1b17054a2151d1df50
clara-cdp <clarianne.cdp@gmail.com>
2026-06-04 13:52:04 +0200

    arranged format

--- frontend/src/components/code-connect/projectTeam/ProjectTeam.tsx
+++ frontend/src/components/code-connect/projectTeam/ProjectTeam.tsx
@@ -24,12 +24,12 @@ function ProjectTeam({
   const { getTeamByRole } = useProjectContributors(contributors);
   const frontendData = getTeamByRole("frontend");
   const backendData = getTeamByRole("backend");
-  const [selectedSlotIndex, setSelectedSlotIndex] = useState < number | null > (
+  const [selectedSlotIndex, setSelectedSlotIndex] = useState<number | null>(
     null,
   );
-  const [selectedRole, setSelectedRole] = useState <
+  const [selectedRole, setSelectedRole] = useState<
     "Frontend Developer" | "Backend Developer" | null
-    > (null);
+  >(null);
 
   const handleSlotClick = (
     role: "Frontend Developer" | "Backend Developer",
