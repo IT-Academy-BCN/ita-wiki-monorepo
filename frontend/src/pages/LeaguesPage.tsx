382e764fb4a66c63203668a7ccfd52b9b30412f9
Daniel Vera <dani.vmg91@gmail.com>
2026-06-04 12:31:15 +0200

    feat: add trigger button to leagues view for admin, superadmin and mentor roles

--- frontend/src/pages/LeaguesPage.tsx
+++ frontend/src/pages/LeaguesPage.tsx
@@ -4,13 +4,25 @@ import LeagueToggle, {
 } from "../components/leagues-ranking/LeagueToggle/LeagueToggle";
 import { GlobalRanking } from "../components/leagues-ranking/GlobalRanking/GlobalRanking";
 import { WeeklyRanking } from "../components/leagues-ranking/WeeklyRanking/WeeklyRanking";
+import { useUser } from "../hooks/useUser";
+import rotateIcon from "../assets/rotate.svg";
 
 const LeaguesPage = () => {
   const [view, setView] = useState<LeagueView>("weekly");
+  const { user } = useUser();
 
   return (
     <div className="px-6 md:px-10 xl:px-20 2xl:px-6 flex flex-col gap-10">
       <LeagueToggle view={view} onChange={setView} />
+      {user?.role && user.role !== "student" && (
+        <div className="flex justify-end">
+          <button
+            onClick={() => { }}
+            className="bg-primary p-1 rounded-md hover:bg-[#a1156a] cursor-pointer">
+            <img src={rotateIcon} alt="Trigger weekly transition" className="w-7 h-7" />
+          </button>
+        </div>
+      )}
       {view === "weekly" ? <WeeklyRanking /> : <GlobalRanking />}
     </div>
   );
