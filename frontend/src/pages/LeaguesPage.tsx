2506fa429799ec5520d4d6d65d433dd1ed555608
Daniel Vera <dani.vmg91@gmail.com>
2026-06-05 12:04:28 +0200

    feat: add confirmation modal for weekly league transition

--- frontend/src/pages/LeaguesPage.tsx
+++ frontend/src/pages/LeaguesPage.tsx
@@ -4,15 +4,32 @@ import LeagueToggle, {
 } from "../components/leagues-ranking/LeagueToggle/LeagueToggle";
 import { GlobalRanking } from "../components/leagues-ranking/GlobalRanking/GlobalRanking";
 import { WeeklyRanking } from "../components/leagues-ranking/WeeklyRanking/WeeklyRanking";
+import GenericModal from "../components/ui/Modal/GenericModal";
 
 const LeaguesPage = () => {
   const [view, setView] = useState<LeagueView>("weekly");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div className="px-6 md:px-10 xl:px-20 2xl:px-6 flex flex-col gap-10">
       <LeagueToggle view={view} onChange={setView} />
+      <button onClick={() => setIsModalOpen(true)}>Trigger</button>
+      <GenericModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title="Actualitzar lligues"
+        showPrimaryButton
+        primaryButtonText="Confirmar"
+        primaryButtonAction={() => setIsModalOpen(false)}
+        showSecondaryButton
+        secondaryButtonText="Cancel·lar"
+        secondaryButtonAction={() => setIsModalOpen(false)}
+      >
+        <p>Vols actualitzar les lligues?</p>
+      </GenericModal>
+
       {view === "weekly" ? <WeeklyRanking /> : <GlobalRanking />}
-    </div>
+    </div >
   );
 };
 
