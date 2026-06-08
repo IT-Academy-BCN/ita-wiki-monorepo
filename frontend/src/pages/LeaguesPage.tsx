d2cb967910e6c5242d70da126b6a815a5b1a9183
Clàudia López Torres <claudia_4bcn@hotmail.com>
2026-06-08 10:22:17 +0200

    Test

--- frontend/src/pages/LeaguesPage.tsx
+++ frontend/src/pages/LeaguesPage.tsx
@@ -4,6 +4,7 @@ import LeagueToggle, {
 } from "../components/leagues-ranking/LeagueToggle/LeagueToggle";
 import { GlobalRanking } from "../components/leagues-ranking/GlobalRanking/GlobalRanking";
 import { WeeklyRanking } from "../components/leagues-ranking/WeeklyRanking/WeeklyRanking";
+import UiButton from "../components/ui/shared-ui/UiButton";
 import GenericModal from "../components/ui/Modal/GenericModal";
 
 const LeaguesPage = () => {
@@ -14,6 +15,9 @@ const LeaguesPage = () => {
     <div className="px-6 md:px-10 xl:px-20 2xl:px-6 flex flex-col gap-10">
       <div className="flex items-center justify-between w-full">
         <LeagueToggle view={view} onChange={setView} />
+        <UiButton variant="link" size="sm" onClick={() => setIsModalOpen(true)}>
+          Veure el meu historial
+        </UiButton>
       </div>
       {view === "weekly" ? <WeeklyRanking /> : <GlobalRanking />}
 
