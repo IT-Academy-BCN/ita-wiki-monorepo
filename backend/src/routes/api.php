786aaa7c8ed09d84a6ea0b4abd9437381262c6de
Miguel Montaño <miiguel2m@gmail.com>
2026-06-08 11:01:28 +0200

    feat: add GET /ligas/history route

--- backend/src/routes/api.php
+++ backend/src/routes/api.php
@@ -182,4 +182,5 @@ Route::get('/ligas/ranking', [LigaController::class, 'ranking'])->name('ligas.ra
 Route::middleware('auth:sanctum')->group(function () {
     Route::put('/ligas/{user}/points', [LigaController::class, 'addPoints'])->name('ligas.points.add');
     Route::post('/ligas', [LigaController::class, 'store'])->name('ligas.store');
+    Route::get('/ligas/history', [LigaController::class, 'history'])->name('ligas.history');
 });
