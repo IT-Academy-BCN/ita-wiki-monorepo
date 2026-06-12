c84acc38f1a74c34fa3e6345dacc963d8cbde590
Daniel Vera <dani.vmg91@gmail.com>
2026-06-12 18:34:19 +0200

    feat: connect trigger button to weekly transition API

--- frontend/src/pages/LeaguesPage.tsx
+++ frontend/src/pages/LeaguesPage.tsx
@@ -9,12 +9,15 @@ import rotateIcon from "../assets/rotate.svg";
 import UiButton from "../components/ui/shared-ui/UiButton";
 import GenericModal from "../components/ui/Modal/GenericModal";
 import PointsHistoryTable from "../components/leagues-ranking/PointsHistoryTable/PointsHistoryTable";
+import { useTriggerWeeklyTransition } from "../hooks/useTriggerWeeklyTransition";
+import { triggerWeeklyTransition } from "../api/endPointLeagues";
 
 const LeaguesPage = () => {
   const [view, setView] = useState<LeagueView>("weekly");
   const { user } = useUser();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isTriggerModalOpen, setIsTriggerModalOpen] = useState(false);
+  const { trigger } = useTriggerWeeklyTransition({triggerWeeklyTransition});
 
   return (
     <div className="px-6 md:px-10 xl:px-20 2xl:px-6 flex flex-col gap-10">
@@ -62,7 +65,7 @@ const LeaguesPage = () => {
         title="Actualitzar lligues"
         showPrimaryButton
         primaryButtonText="Confirmar"
-        primaryButtonAction={() => setIsTriggerModalOpen(false)}
+        primaryButtonAction={async () => { await trigger(); setIsTriggerModalOpen(false); }}
         showSecondaryButton
         secondaryButtonText="Cancel·lar"
         secondaryButtonAction={() => setIsTriggerModalOpen(false)}
