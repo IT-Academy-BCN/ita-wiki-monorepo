0cc07a8fed3cd4200cafcde2c06948bfc013e1b3
orlandorojasoyarzun <orlandorojasoyarzun@gmail.com>
2026-06-12 13:29:45 +0200

    prettier formated

--- frontend/src/components/tickets/CategoryIcon.tsx
+++ frontend/src/components/tickets/CategoryIcon.tsx
@@ -4,14 +4,14 @@ import ErrorIcon from "../../assets/ticketing-category-error-icon.svg";
 import SuggerimentIcon from "../../assets/ticketing-category-suggeriment-icon.svg";
 import AltreIcon from "../../assets/ticketing-category-altre-icon.svg";
 interface CategoryIconProps {
-    category: TicketCategoryEnum;
+  category: TicketCategoryEnum;
 }
 const iconMap: Record<TicketCategoryEnum, string> = {
-    [TicketCategoryEnum.BUG]: ErrorIcon,
-    [TicketCategoryEnum.SUGGESTION]: SuggerimentIcon,
-    [TicketCategoryEnum.OTHER]: AltreIcon,
+  [TicketCategoryEnum.BUG]: ErrorIcon,
+  [TicketCategoryEnum.SUGGESTION]: SuggerimentIcon,
+  [TicketCategoryEnum.OTHER]: AltreIcon,
 };
 const CategoryIcon: FC<CategoryIconProps> = ({ category }) => (
-    <img src={iconMap[category]} alt={category} className="w-6 h-6" />
+  <img src={iconMap[category]} alt={category} className="w-6 h-6" />
 );
 export default CategoryIcon;
