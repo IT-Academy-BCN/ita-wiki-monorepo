9b4d2e7329c8c35b4f71de90dfaba079a8ca48c0
Daniel Vera <dani.vmg91@gmail.com>
2026-06-13 15:28:23 +0200

    feat(#795) reactively update weekly ranking after trigger

--- frontend/src/pages/LeaguesPage.tsx
+++ frontend/src/pages/LeaguesPage.tsx
@@ -9,13 +9,17 @@ import rotateIcon from "../assets/rotate.svg";
 import UiButton from "../components/ui/shared-ui/UiButton";
 import GenericModal from "../components/ui/Modal/GenericModal";
 import PointsHistoryTable from "../components/leagues-ranking/PointsHistoryTable/PointsHistoryTable";
+import { useTriggerWeeklyTransition } from "../hooks/useTriggerWeeklyTransition";
+import { triggerWeeklyTransition } from "../api/endPointLeagues";
+import { useLeagues } from "../hooks/useLeagues";
 
 const LeaguesPage = () => {
   const [view, setView] = useState<LeagueView>("weekly");
   const { user } = useUser();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isTriggerModalOpen, setIsTriggerModalOpen] = useState(false);
-
+  const { trigger } = useTriggerWeeklyTransition({ triggerWeeklyTransition });
+  const { leagues, fetchLeagues } = useLeagues();
   return (
     <div className="px-6 md:px-10 xl:px-20 2xl:px-6 flex flex-col gap-10">
       <div className="max-w-3xl w-full">
@@ -45,7 +49,11 @@ const LeaguesPage = () => {
         </div>
       </div>
 
-      {view === "weekly" ? <WeeklyRanking /> : <GlobalRanking />}
+      {view === "weekly" ? (
+        <WeeklyRanking leagues={leagues} />
+      ) : (
+        <GlobalRanking />
+      )}
 
       <GenericModal
         isOpen={isModalOpen}
@@ -62,7 +70,11 @@ const LeaguesPage = () => {
         title="Actualitzar lligues"
         showPrimaryButton
         primaryButtonText="Confirmar"
-        primaryButtonAction={() => setIsTriggerModalOpen(false)}
+        primaryButtonAction={async () => {
+          await trigger();
+          await fetchLeagues();
+          setIsTriggerModalOpen(false);
+        }}
         showSecondaryButton
         secondaryButtonText="Cancel·lar"
         secondaryButtonAction={() => setIsTriggerModalOpen(false)}
